3900cf3befe5dbb5aff3d8758620c0441a0b0ae2
vipinkumartablet-sketch <vipinkumartablet@gmail.com>
2026-06-22 15:12:14 +0530

    feat: create register page UI

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,60 @@
 function Register() {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <p>Create a new account</p>
+    <div
+      style={{
+        maxWidth: "400px",
+        margin: "50px auto",
+        textAlign: "center",
+      }}
+    >
+      <h1>Register</h1>
+
+      <form>
+        <div>
+          <label>Name</label>
+          <br />
+          <input type="text" />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Email</label>
+          <br />
+          <input
+            type="email"
+        style={{
+         width: "100%",
+            padding: "10px",
+    }}
+/>
+        </div>
+
+        <br />
+
+        <div>
+          <label>Password</label>
+          <br />
+          <input type="password" />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Role</label>
+          <br />
+          <select>
+            <option>Student</option>
+            <option>Organizer</option>
+          </select>
+        </div>
+
+        <br />
+
+        <button type="submit">
+          Register
+        </button>
+      </form>
     </div>
   );
 }
